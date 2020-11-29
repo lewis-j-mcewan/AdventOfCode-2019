521ad6026ae8e89a027659155c4b64040a48bbcf
Lewis McEwan <lewis.mcewan@fmg.co.nz>
2020-11-30 10:41:45 +1300

    Completed Day 1 part two

--- src/Day1.java
+++ src/Day1.java
@@ -4,13 +4,21 @@ public class Day1 {
 
     public static void main(String[] args) throws Exception {
         
-        List<String> input = LoadFile.FileToString("resources/Day1.txt");
-        System.out.println(input);
+        List<String> input = LoadFile.FileToString("Day1/resources/Day1.txt");
+        if(input.isEmpty() || input == null){
+            return;
+        }
+
         int sum = 0;
         for(String s : input){
             int mass = Integer.parseInt(s);
-            int result = (mass/3)-2;
-            sum += result;
+            while(mass > 0){
+                int fuel = (mass/3)-2;
+                if(fuel > 0){
+                    sum += fuel;
+                }
+                mass = fuel;
+            }
         }
         System.out.println(sum);
     }
